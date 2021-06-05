bb5c82a6943b94a3ce6f877effa2955fe48607ae
Camilo Romero <militoromero10@gmail.com>
2021-06-05 16:07:20 -0500

    init project, model added and embbeded activemq server

---
+++ src/main/java/com/milo/jms/MiloJmsApplication.java
@@ -0,0 +1,26 @@
+package com.milo.jms;
+
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloJmsApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        //servidor activemq embebido
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));
+
+        server.start();
+
+        SpringApplication.run(MiloJmsApplication.class, args);
+    }
+
+}
