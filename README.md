2b730530952a16a82a20fdd852140ccbae4b245e
Camilo Romero <militoromero10@gmail.com>
2021-06-05 19:12:01 -0500

    adding support for activemq artemisa

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # milo-jms
+
+# Usando ActiveMQ Artemisa Dockerizado
+___
+- docker pull vromero/activemq-artemis
+- docker run -it --rm \
+  -p 8161:8161 \
+  -p 61616:61616 \
+  vromero/activemq-simetraehcapa
+---
+agregar application properties o variable de entorno en docker 
+
+-e ARTEMIS_USERNAME=artemis \
+-e ARTEMIS_PASSWORD=simetraehcapa \
+___
--- src/main/java/com/milo/jms/MiloJmsApplication.java
+++ src/main/java/com/milo/jms/MiloJmsApplication.java
@@ -1,8 +1,8 @@
 package com.milo.jms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.server.ActiveMQServer;
+//import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,13 +14,13 @@ public class MiloJmsApplication {
         //servidor activemq embebido
         //inicio
         //volar esto si contamos con un broker **dockerisar un activemq ejemplo**
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+      /*  ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
                 .setPersistenceEnabled(false)
                 .setJournalDirectory("target/data/journal")
                 .setSecurityEnabled(false)
                 .addAcceptorConfiguration("invm", "vm://0"));
 
-        server.start();
+        server.start();*/
         //fin
         SpringApplication.run(MiloJmsApplication.class, args);
     }
